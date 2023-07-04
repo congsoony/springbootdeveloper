a9f9e4207dc04b2a6b7997b279d864bcfa5f5ba5
leejaseop <vavonae@gmail.com>
2023-07-04 18:28:46 +0900

    파일 위치 옮기기

--- springbootinit/src/main/java/org/example/springbootdeveloper/Member.java
+++ springbootinit/src/main/java/org/example/springbootdeveloper/Member.java
@@ -1,11 +1,12 @@
 package org.example.springbootdeveloper;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
 @Entity
